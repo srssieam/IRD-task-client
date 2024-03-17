e71739eeae1769f2522c722c3d24dd1f7fa3fa14
srs-sieam <srssieam@gmail.com>
2024-03-17 10:59:17 +0600

    make some changes in design and display duas according to category and sub category

--- src/app/duas/[catName]/page.jsx
+++ src/app/duas/[catName]/page.jsx
@@ -1,12 +1,78 @@
 "use client"
 import usePublicAPI from '@/hooks/usePublicAPI';
-const DuaDetails = ({params, searchParams}) => {
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import card from "@/assets/duacard.svg"
+import { IoCopyOutline } from "react-icons/io5";
+import { BsBookmark, BsShare } from "react-icons/bs";
+import { PiLightbulbFilament } from "react-icons/pi";
+import { MdReportGmailerrorred } from "react-icons/md";
+import "./tooltip.css"
+
+const DuaDetails = ({ params, searchParams }) => {
     const publicAPI = usePublicAPI();
-    
+    const [duaData, setDuaData] = useState();
+    const [subCategory, setSubCategory] = useState([])
+
+    useEffect(() => {
+        publicAPI.get(`/duas/${searchParams.cat}`)
+            .then(res => setDuaData(res.data))
+    }, [publicAPI, searchParams.cat])
+
+
+    useEffect(() => {
+        publicAPI.get(`/sub-category/${searchParams.cat}`)
+            .then(res => setSubCategory(res?.data))
+    }, [searchParams.cat, publicAPI])
+
     return (
-        <div>
-            <h1><span className='text-green-600 font-semibold'>Section:</span></h1>
-            <h1>{searchParams.cat}</h1>
+        <div className='max-h-[85vh] overflow-y-auto font-poppins'>
+
+            {
+                subCategory.map(subcat => (
+                    <div key={subcat.id} className='flex flex-col gap-6 mb-7'>
+                        <h1 className='bg-white px-6 py-4 rounded-xl'><span className='text-[#1FA45B] font-semibold'>Section:</span>{subcat.subcat_name_en}</h1>
+                        {
+                            duaData?.filter(findDua => findDua.subcat_id === subcat.subcat_id).map(dua => (
+                                <div id={`subcat=${dua.subcat_id}`} key={dua.id} className='flex flex-col gap-6'>
+                                    <div id={`subcat=${dua.subcat_id}#dua=${dua.dua_id}`} className='bg-white px-6 py-4 rounded-xl space-y-5'>
+                                        <div className='flex gap-3 items-center text-[#1FA45B] font-semibold'>
+                                            <Image src={card} alt="logo" className="h-8 w-8"></Image>
+                                            <h1>{dua.dua_id}. {dua.dua_name_en}</h1>
+                                        </div>
+                                        <div className='space-y-6'>
+                                            {dua.top_en && <p className='text-lg'>{dua.top_en}</p>}
+                                            {dua.dua_arabic && <p className='text-3xl text-right font-poppins leading-loose'>{dua.dua_arabic}</p>}
+                                            {dua.transliteration_en && <p className='text-lg italic'><strong>Transliteration: </strong>{dua.transliteration_en}</p>}
+                                            {dua.translation_en && <p className='text-lg text-zinc-600'><strong>Translation: </strong>{dua.translation_en}</p>}
+                                            {dua.bottom_en && <p className='text-lg'>{dua.bottom_en}</p>}
+                                            {dua.refference_en && <p className='text-lg text-zinc-700 font-semibold'><span className='text-[#1FA45B] text-xl'>Reference:</span><br />{dua.refference_en}</p>}
+                                        </div>
+                                        <div className='flex justify-between items-center'>
+                                            {
+                                                dua.audio ? <audio controls>
+                                                    <source src={dua.audio} type="audio/mpeg" />
+                                                    Your browser does not support the audio element.
+                                                </audio> :
+                                                <div></div>
+                                            }
+                                            <div className='flex gap-7'>
+                                                <button className='relative button'><IoCopyOutline className='text-zinc-600 text-lg' /><span className='absolute showTooltip bg-zinc-800 text-white py-1 px-2 rounded-md -top-9 -right-3 opacity-0 '>copy</span></button>
+                                                <button className='relative button'><BsBookmark className='text-zinc-600 text-lg' /><span className='absolute showTooltip bg-zinc-800 text-white py-1 px-2 rounded-md -top-9 -right-8 opacity-0 '>Bookmark</span></button>
+                                                <button className='relative button'><PiLightbulbFilament className='text-zinc-600 text-2xl' /><span className='absolute showTooltip bg-zinc-800 text-white py-1 px-2 rounded-md -top-9 -right-8 opacity-0 '>Memorize</span></button>
+                                                <button className='relative button'><BsShare className='text-zinc-600 text-lg' /><span className='absolute showTooltip bg-zinc-800 text-white py-1 px-2 rounded-md -top-9 -right-6 opacity-0 '>share</span></button>
+                                                <button className='relative button'><MdReportGmailerrorred className='text-zinc-600 text-2xl' /><span className='absolute showTooltip bg-zinc-800 text-white py-1 px-2 rounded-md -top-9 -right-5 opacity-0 '>report</span></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                ))
+            }
+
+
         </div>
     );
 };
--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -10,36 +10,54 @@ import { usePathname } from 'next/navigation';
 const CategorySection = () => {
     const [category, setCategory] = useState()
     const [subCategory, setSubCategory] = useState([])
+    const [duas, setDuas] = useState([])
+    const [openSubCat, setOpenSubCat] = useState()
     const publicAPI = usePublicAPI();
+
+    // fetch category data
     useEffect(() => {
         publicAPI.get('/category-data')
             .then(res => setCategory(res.data))
     }, [publicAPI])
 
 
-    useEffect(()=>{
+    // fetch sub category data
+    useEffect(() => {
         publicAPI.get(`/sub-category-data`)
-            .then(res=> setSubCategory(res.data))
-    },[, publicAPI])
+            .then(res => setSubCategory(res.data))
+    }, [, publicAPI])
+
+    // fetch dua data
+    useEffect(() => {
+        publicAPI.get(`/dua-data`)
+            .then(res => setDuas(res.data))
+    }, [, publicAPI])
 
     const pathName = usePathname();
-    console.log(pathName)
+    // console.log(pathName)
+
 
 
     return (
-        <div className=''>
+        <div>
             <h1 className='bg-[#1FA45B] text-white p-4 text-center '>Categories</h1>
             <div className='p-3'>
+
+                {/* category searchbar */}
                 <form className='relative'>
                     <GoSearch className='absolute text-xl text-gray-500  top-4 left-4' />
                     <input type="text" name="search" placeholder='Search Categories' id="" className='text-sm w-full outline-none border border-gray-200 pl-14 p-4 rounded-md focus:border-[3px] focus:border-green-700' />
                 </form>
+
+                {/* categories */}
                 <div className='flex flex-col gap-2 rounded-md overflow-y-scroll max-h-[68vh] py-4'>
                     {
                         category?.map(cat => (
                             <div key={cat.id} className='flex flex-col'>
+
+                                {/* category */}
                                 <Link href={`/duas/${cat.cat_name_en.replace(/\s+/g, '-').toLowerCase()}?cat=${cat.cat_id}`}>
-                                    <div className={`hover:bg-[#E8F0F5] p-2 flex justify-between items-center rounded-md ${pathName === `/duas/${cat.cat_name_en.replace(/\s+/g, '-').toLowerCase()}`? "bg-[#E8F0F5]": ""}`}>
+                                    <div className={`hover:bg-[#E8F0F5] p-2 flex justify-between items-center rounded-md ${pathName === `/duas/${cat.cat_name_en.replace(/\s+/g, '-').toLowerCase()}` ? "bg-[#E8F0F5]" : ""}`}>
                                         <div className='flex gap-2 items-center'>
                                             <Image src={catIcon} alt="logo" className="h-14 w-14 p-2 rounded-md bg-[#CFE0E5]"></Image>
                                             <div>
@@ -53,15 +71,32 @@ const CategorySection = () => {
                                         </div>
                                     </div>
                                 </Link>
-                                <div className={`${pathName === `/duas/${cat.cat_name_en.replace(/\s+/g, '-').toLowerCase()}`? "block": "hidden"}`}>
+
+                                {/* sub category */}
+                                <div className={`${pathName === `/duas/${cat.cat_name_en.replace(/\s+/g, '-').toLowerCase()}` ? "block ml-7 p-4 border-l-2 border-l-green-700 border-dotted space-y-5" : "hidden"}`}>
                                     {
-                                        subCategory.filter(subCat => subCat.cat_id === cat.cat_id).map(sub =>(
-                                            <Link href={`/duas/${cat.cat_name_en.replace(/\s+/g, '-').toLowerCase()}?cat=${sub.cat_id}&subcat=${sub.subcat_id}`} key={sub.id} >
-                                            <div className='relative text-sm ml-5 p-4 border-l-2 border-l-green-700 border-dotted '>
-                                                <div className='absolute -left-1 top-5 bg-green-600 w-2 h-2 rounded-full'></div>
-                                                <h1>{sub.subcat_name_en}</h1>
+                                        subCategory.filter(subCat => subCat.cat_id === cat.cat_id).map(sub => (
+                                            <div  key={sub.id + "sub"}>
+                                                <div className='relative text-sm '>
+                                                    <Link onClick={()=>setOpenSubCat(sub.subcat_id)} href={`/duas/${cat.cat_name_en.replace(/\s+/g, '-').toLowerCase()}?cat=${sub.cat_id}#subcat=${sub.subcat_id}`}  >
+                                                        <div className='absolute -left-5 top-1 bg-green-600 w-2 h-2 rounded-full'></div>
+                                                        <h1 className='text-zinc-700 font-semibold'>{sub.subcat_name_en}</h1>
+                                                    </Link>
+                                                </div>
+                                                <div className={`space-y-5 mt-5`}>
+
+                                                    {/* dua */}
+                                                    {
+                                                        duas.filter(dua => sub.subcat_id === dua.subcat_id).map(singleDua => (
+                                                            <div key={singleDua.id + "dua"} className={`ml-5 text-sm ${singleDua.subcat_id === openSubCat ? "block" : "hidden"}`}>
+                                                                <Link href={`/duas/${cat.cat_name_en.replace(/\s+/g, '-').toLowerCase()}?cat=${singleDua.cat_id}#subcat=${singleDua.subcat_id}#dua=${singleDua.dua_id}`}>
+                                                                    &gt; {singleDua.dua_name_en }
+                                                                </Link>
+                                                            </div>
+                                                        ))
+                                                    }
+                                                </div>
                                             </div>
-                                            </Link>
                                         ))
                                     }
                                 </div>
